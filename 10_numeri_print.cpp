7ff44555aa0623c8694815b5a34c32c67d73b16e
mattiasan <115842434+mattiasaan@users.noreply.github.com>
2025-01-31 10:04:32 +0100

    Add files via upload

---
+++ 10_numeri_print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "0";
+cout << "1";
+cout << "2";
+cout << "3";
+cout << "4";
+cout << "5";
+cout << "6";
+cout << "7";
+cout << "8";
+cout << "9";
+
+return 0;
+}
+
